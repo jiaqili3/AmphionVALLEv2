b228ac2badefb6599de19cf7040b17aaf7b90d70
HarryHe11 <hehaorui11@gmail.com>
2024-06-17 17:30:47 +0800

    add back data preprocessing options

--- bins/tts/train.py
+++ bins/tts/train.py
@@ -97,6 +97,29 @@ def main():
     # VALLETrainer.add_arguments(parser)
     args = parser.parse_args()
     cfg = load_config(args.config)
+    
+    # Data Augmentation
+    if hasattr(cfg, 'preprocess'):
+        if hasattr(cfg.preprocess, "data_augment"):
+            if (
+                type(cfg.preprocess.data_augment) == list
+                and len(cfg.preprocess.data_augment) > 0
+            ):
+                new_datasets_list = []
+                for dataset in cfg.preprocess.data_augment:
+                    new_datasets = [
+                        f"{dataset}_pitch_shift" if cfg.preprocess.use_pitch_shift else None,
+                        (
+                            f"{dataset}_formant_shift"
+                            if cfg.preprocess.use_formant_shift
+                            else None
+                        ),
+                        f"{dataset}_equalizer" if cfg.preprocess.use_equalizer else None,
+                        f"{dataset}_time_stretch" if cfg.preprocess.use_time_stretch else None,
+                    ]
+                    new_datasets_list.extend(filter(None, new_datasets))
+                cfg.dataset.extend(new_datasets_list)
+
     print("experiment name: ", args.exp_name)
     # # CUDA settings
     cuda_relevant()
